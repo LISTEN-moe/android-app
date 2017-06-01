4a0989104d1b4d8005f62810c93a71bbc681ef92
Eugene Cheung <ey.cheung@outlook.com>
2017-06-01 18:01:01 -0400

    Minor fixes

--- app/src/main/java/jcotter/listenmoe/model/UserInfo.java
+++ app/src/main/java/jcotter/listenmoe/model/UserInfo.java
@@ -3,6 +3,7 @@ package jcotter.listenmoe.model;
 public class UserInfo {
     private int id;
     private String username;
+    // private Object permissions;
 
     public UserInfo(int id, String username) {
         this.id = id;
