47a926f21c94b77bec7cda2cf33dc590eaa01e4c
Eugene Cheung <ey.cheung@outlook.com>
2017-08-14 19:56:03 -0400

    Swipe away notification when not playing

--- app/src/main/java/me/echeung/moemoekyun/service/notification/AppNotification.java
+++ app/src/main/java/me/echeung/moemoekyun/service/notification/AppNotification.java
@@ -1,6 +1,9 @@
 package me.echeung.moemoekyun.service.notification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -18,12 +21,13 @@ public class AppNotification {
 
     private static final String NOTIFICATION_CHANNEL = "notif_channel";
     private static final int NOTIFICATION_ID = 1;
-    private StreamService service;
 
-    private int actionRequestCode;
+    private StreamService service;
+    private NotificationManager notificationManager;
 
     public AppNotification(StreamService service) {
         this.service = service;
+        notificationManager = (NotificationManager) service.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     public void update() {
@@ -39,13 +43,11 @@ public class AppNotification {
         final boolean isPlaying = service.isPlaying();
         final String text = song.getArtistAndAnime();
 
-        actionRequestCode = 0;
-
         // Play/pause action
         final NotificationCompat.Action playPauseAction = new NotificationCompat.Action(
                 isPlaying ? R.drawable.ic_pause_white_24dp : R.drawable.ic_play_arrow_white_24dp,
                 isPlaying ? service.getString(R.string.action_pause) : service.getString(R.string.action_play),
-                getPlaybackActionService(StreamService.class, StreamService.PLAY_PAUSE)
+                getPlaybackActionService(StreamService.PLAY_PAUSE)
         );
 
         // Favorite action
@@ -54,28 +56,22 @@ public class AppNotification {
             favoriteAction = new NotificationCompat.Action(
                     song.isFavorite() ? R.drawable.ic_star_white_24dp : R.drawable.ic_star_border_white_24dp,
                     song.isFavorite() ? service.getString(R.string.action_unfavorite) : service.getString(R.string.action_favorite),
-                    getPlaybackActionService(StreamService.class, StreamService.TOGGLE_FAVORITE)
+                    getPlaybackActionService(StreamService.TOGGLE_FAVORITE)
             );
         } else {
             favoriteAction = new NotificationCompat.Action(
                     R.drawable.ic_star_border_white_24dp,
                     service.getString(R.string.action_favorite),
-                    getPlaybackActionActivity(MainActivity.class, MainActivity.TRIGGER_LOGIN_AND_FAVORITE)
+                    getPlaybackActionActivity(MainActivity.TRIGGER_LOGIN_AND_FAVORITE)
             );
         }
-
-        // Stop action
-        final NotificationCompat.Action stopAction = new NotificationCompat.Action(
-                R.drawable.ic_stop_white_24dp,
-                service.getString(R.string.action_stop),
-                getPlaybackActionService(StreamService.class, StreamService.STOP)
-        );
 
         // Build the notification
         final Intent action = new Intent(service, MainActivity.class);
         action.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
         final PendingIntent clickIntent = PendingIntent.getActivity(service, 0, action, PendingIntent.FLAG_UPDATE_CURRENT);
+        final PendingIntent deleteIntent = getPlaybackActionService(StreamService.STOP);
 
         final NotificationCompat.Builder builder = new NotificationCompat.Builder(service, NOTIFICATION_CHANNEL)
                 .setSmallIcon(R.drawable.icon_notification)
@@ -85,32 +81,39 @@ public class AppNotification {
                 .setCategory(NotificationCompat.CATEGORY_SERVICE)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setContentIntent(clickIntent)
-                .setOngoing(service.isStreamStarted())
+                .setDeleteIntent(deleteIntent)
+                .setOngoing(isPlaying)
                 .setShowWhen(false)
                 .addAction(playPauseAction)
-                .addAction(favoriteAction)
-                .addAction(stopAction);
+                .addAction(favoriteAction);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
-            builder.setStyle(new MediaStyle().setShowActionsInCompactView(0, 1, 2));
+            builder.setStyle(new MediaStyle().setShowActionsInCompactView(0, 1));
             builder.setColor(ContextCompat.getColor(service, R.color.colorAccent));
         }
 
-        service.startForeground(NOTIFICATION_ID, builder.build());
+        final Notification notification = builder.build();
+
+        if (isPlaying) {
+            service.startForeground(NOTIFICATION_ID, notification);
+        } else {
+            service.stopForeground(false);
+            notificationManager.notify(NOTIFICATION_ID, notification);
+        }
     }
 
-    private PendingIntent getPlaybackActionService(final Class target, final String action) {
-        final Intent intent = new Intent(service, target);
+    private PendingIntent getPlaybackActionService(final String action) {
+        final Intent intent = new Intent(service, StreamService.class);
         intent.setAction(action);
 
-        return PendingIntent.getService(service, ++actionRequestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getService(service, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
-    private PendingIntent getPlaybackActionActivity(final Class target, final String action) {
-        final Intent intent = new Intent(service, target);
+    private PendingIntent getPlaybackActionActivity(final String action) {
+        final Intent intent = new Intent(service, MainActivity.class);
         intent.setAction(action);
 
-        return PendingIntent.getActivity(service, ++actionRequestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getActivity(service, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
