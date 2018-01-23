ab6c395d7113eeb302a2a25d14187e788fa3b681
Eugene Cheung <ey.cheung@outlook.com>
2018-01-22 19:09:40 -0500

    Null check

--- app/src/main/java/me/echeung/moemoekyun/utils/ImeUtil.java
+++ app/src/main/java/me/echeung/moemoekyun/utils/ImeUtil.java
@@ -6,6 +6,10 @@ import android.view.inputmethod.InputMethodManager;
 public final class ImeUtil {
 
     public static void hideSoftKeyboard(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+
         final InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         if (inputMethodManager != null && inputMethodManager.isActive() && activity.getCurrentFocus() != null) {
             inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
