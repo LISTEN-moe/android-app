10ecc0f6890d74d428142fbf01603cd01b6cc488
Eugene Cheung <ey.cheung@outlook.com>
2018-03-14 20:06:48 -0400

    Events are always objects now

--- listenmoe-api/src/main/java/me/echeung/listenmoeapi/models/Event.java
+++ listenmoe-api/src/main/java/me/echeung/listenmoeapi/models/Event.java
@@ -1,10 +1,8 @@
 package me.echeung.listenmoeapi.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Event {
     private String name;
     private String image;
--- listenmoe-api/src/main/java/me/echeung/listenmoeapi/responses/socket/SocketUpdateResponse.java
+++ listenmoe-api/src/main/java/me/echeung/listenmoeapi/responses/socket/SocketUpdateResponse.java
@@ -1,17 +1,8 @@
 package me.echeung.listenmoeapi.responses.socket;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-
-import java.lang.reflect.Type;
 import java.util.List;
 
 import lombok.Getter;
-import lombok.Setter;
 import me.echeung.listenmoeapi.models.Event;
 import me.echeung.listenmoeapi.models.Song;
 import me.echeung.listenmoeapi.models.User;
@@ -29,7 +20,6 @@ public class SocketUpdateResponse extends SocketBaseResponse {
         private Queue queue;
         private int listeners;
         private User requester;
-        @Setter
         private Event event;
     }
 
@@ -38,25 +28,5 @@ public class SocketUpdateResponse extends SocketBaseResponse {
         private int inQueue;
         private int inQueueByUser;
         private int inQueueBeforeUser;
-    }
-
-    // TODO: remove this once we're safely at a point where the app won't be handling both
-    // string and object events
-    public static class DetailsDeserializer implements JsonDeserializer<Details> {
-
-        @Override
-        public Details deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
-            Details details = new Gson().fromJson(json, Details.class);
-            JsonObject jsonObject = json.getAsJsonObject();
-
-            if (jsonObject.has("event")) {
-                JsonElement elem = jsonObject.get("event");
-                if (elem != null && elem.isJsonPrimitive()) {
-                    details.setEvent(new Event(elem.getAsString(), null));
-                }
-            }
-
-            return details;
-        }
     }
 }
