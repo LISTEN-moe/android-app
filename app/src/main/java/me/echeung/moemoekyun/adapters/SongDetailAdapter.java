bd957f9ac8ed3ed46e6ff7bd37da139309c2c6e6
Eugene Cheung <ey.cheung@outlook.com>
2018-03-18 22:22:55 -0400

    Share common song item layout

--- app/src/main/java/me/echeung/moemoekyun/adapters/SongDetailAdapter.java
+++ app/src/main/java/me/echeung/moemoekyun/adapters/SongDetailAdapter.java
@@ -1,72 +1,36 @@
 package me.echeung.moemoekyun.adapters;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
 import me.echeung.listenmoeapi.models.Song;
 import me.echeung.moemoekyun.R;
+import me.echeung.moemoekyun.databinding.SongItemBinding;
 
 public class SongDetailAdapter extends ArrayAdapter<Song> {
 
     public SongDetailAdapter(@NonNull Context context, List<Song> songs) {
-        super(context, R.layout.song_detail_item, songs);
+        super(context, 0, songs);
     }
 
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            viewHolder = new ViewHolder();
-
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.song_detail_item, parent, false);
-
-            viewHolder.title = convertView.findViewById(R.id.title);
-            viewHolder.artist = convertView.findViewById(R.id.artist);
-            viewHolder.albumArt = convertView.findViewById(R.id.album_art);
-
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
+        final LayoutInflater inflater = LayoutInflater.from(getContext());
+        final SongItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.song_item, parent, false);
 
         final Song song = getItem(position);
-        viewHolder.title.setText(song.getTitle());
-        viewHolder.artist.setText(song.getArtistString());
+        binding.setSong(song);
+        binding.setDetailed(true);
 
-        if (!TextUtils.isEmpty(song.getAlbumArtUrl())) {
-            Glide.with(getContext())
-                    .asBitmap()
-                    .load(song.getAlbumArtUrl())
-                    .apply(new RequestOptions()
-                            .diskCacheStrategy(DiskCacheStrategy.NONE)
-                            .centerCrop())
-                    .into(viewHolder.albumArt);
-        } else {
-            viewHolder.albumArt.setBackgroundResource(R.drawable.blank);
-        }
-
-        return convertView;
-    }
-
-    private static class ViewHolder {
-        TextView title;
-        TextView artist;
-        ImageView albumArt;
+        return binding.getRoot();
     }
 
 }
