8b75615e2903bdc972d888bc83190763423bd4f7
Eugene Cheung <ey.cheung@outlook.com>
2018-03-18 22:05:36 -0400

    Use a view holder for song details list

--- app/src/main/java/me/echeung/moemoekyun/adapters/SongDetailAdapter.java
+++ app/src/main/java/me/echeung/moemoekyun/adapters/SongDetailAdapter.java
@@ -2,11 +2,17 @@ package me.echeung.moemoekyun.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
@@ -16,22 +22,51 @@ import me.echeung.moemoekyun.R;
 public class SongDetailAdapter extends ArrayAdapter<Song> {
 
     public SongDetailAdapter(@NonNull Context context, List<Song> songs) {
-        super(context, 0, songs);
+        super(context, R.layout.song_detail_item, songs);
     }
 
     @NonNull
     @Override
-    public View getView(int position, View view, @NonNull ViewGroup parent) {
-        if (view == null) {
-            view = LayoutInflater.from(getContext()).inflate(R.layout.song_detail_item, parent, false);
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.song_detail_item, parent, false);
+
+            viewHolder.title = convertView.findViewById(R.id.title);
+            viewHolder.artist = convertView.findViewById(R.id.artist);
+            viewHolder.albumArt = convertView.findViewById(R.id.album_art);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         final Song song = getItem(position);
+        viewHolder.title.setText(song.getTitle());
+        viewHolder.artist.setText(song.getArtistString());
 
-        ((TextView) view.findViewById(R.id.title)).setText(song.getTitle());
-        ((TextView) view.findViewById(R.id.artist)).setText(song.getArtistString());
+        if (!TextUtils.isEmpty(song.getAlbumArtUrl())) {
+            Glide.with(getContext())
+                    .asBitmap()
+                    .load(song.getAlbumArtUrl())
+                    .apply(new RequestOptions()
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .centerCrop())
+                    .into(viewHolder.albumArt);
+        } else {
+            viewHolder.albumArt.setBackgroundResource(R.drawable.blank);
+        }
 
-        return view;
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView artist;
+        ImageView albumArt;
     }
 
 }
