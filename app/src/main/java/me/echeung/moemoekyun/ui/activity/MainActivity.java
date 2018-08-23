e4a90982d83d2948f8a5edd20fa8b2a33f9e4e52
Eugene Cheung <ey.cheung@outlook.com>
2018-08-22 23:09:40 -0400

    Inflate menu in now playing sheet (station state is broken)

--- app/src/main/java/me/echeung/moemoekyun/ui/activity/MainActivity.java
+++ app/src/main/java/me/echeung/moemoekyun/ui/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
 import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -164,6 +165,11 @@ public class MainActivity extends BaseActivity {
 
     private void initNowPlaying() {
         nowPlayingSheet = BottomSheetBehavior.from(binding.nowPlaying.nowPlayingSheet);
+
+        // Menu
+        Toolbar toolbar = binding.nowPlaying.toolbar;
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(this::onOptionsItemSelected);
 
         // Restore previous expanded state
         if (App.getPreferenceUtil().isNowPlayingExpanded()) {
