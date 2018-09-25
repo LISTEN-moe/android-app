825cecda2a7989dd4296c1be2d33cde0312866ad
Eugene Cheung <ey.cheung@outlook.com>
2018-09-25 18:55:17 -0400

    Fix some AndroidX imports

--- app/src/main/java/me/echeung/moemoekyun/service/AutoMediaBrowserService.java
+++ app/src/main/java/me/echeung/moemoekyun/service/AutoMediaBrowserService.java
@@ -8,16 +8,16 @@ import android.content.ServiceConnection;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.media.MediaBrowserCompat;
-import android.support.v4.media.MediaBrowserServiceCompat;
 import android.support.v4.media.MediaDescriptionCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.media.MediaBrowserServiceCompat;
 import me.echeung.moemoekyun.App;
 import me.echeung.moemoekyun.R;
 
@@ -44,7 +44,7 @@ public class AutoMediaBrowserService extends MediaBrowserServiceCompat implement
     }
 
     @Override
-    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+    public void onLoadChildren(@NonNull String parentId, @NonNull MediaBrowserServiceCompat.Result<List<MediaBrowserCompat.MediaItem>> result) {
         List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
 
         mediaItems.add(createPlayableMediaItem(RadioService.LIBRARY_JPOP, getResources().getString(R.string.jpop)));
