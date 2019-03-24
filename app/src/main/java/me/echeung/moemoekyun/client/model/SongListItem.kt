b2086b7a92c9c9c2c4939b2cd44153c08d852886
Eugene Cheung <ey.cheung@outlook.com>
2019-03-24 18:47:37 -0400

    Remove unused fields, refactor SongListItem

--- app/src/main/java/me/echeung/moemoekyun/client/model/SongListItem.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/model/SongListItem.kt
@@ -1,143 +1,73 @@
 package me.echeung.moemoekyun.client.model
 
-import java.util.ArrayList
-
 class SongListItem {
+    var id: Int = 0
+    var title: String? = null
+    var titleRomaji: String? = null
+    var titleSearchRomaji: String? = null
+    var artists: List<String>? = null
+    var artistsId: List<Int>? = null
+    var artistsRomaji: List<String>? = null
+    var artistsSearchRomaji: List<String>? = null
     var albums: List<String>? = null
     var albumsId: List<Int>? = null
     var albumsCover: List<String>? = null
     var albumsRomaji: List<String>? = null
     var albumsSearchRomaji: List<String>? = null
-    var artists: List<String>? = null
-    var artistsId: List<Int>? = null
-    var artistsRomaji: List<String>? = null
-    var artistsSearchRomaji: List<String>? = null
-    var duration: Int = 0
-    var enabled: Boolean = false
-    var favorite: Boolean = false
-    var groups: List<String>? = null
-    var id: Int = 0
     var sources: List<String>? = null
     var sourcesRomaji: List<String>? = null
-    var tags: List<String>? = null
-    var title: String? = null
-    var titleRomaji: String? = null
-    var titleSearchRomaji: String? = null
+    var duration: Int = 0
+    var favorite: Boolean = false
 
     fun search(query: String?): Boolean {
         if (query.isNullOrEmpty()) {
             return true
         }
 
-        val query = query.toLowerCase().trim()
-
-        if (title != null && title!!.toLowerCase().contains(query)) {
-            return true
-        }
-
-        if (titleRomaji != null && titleRomaji!!.toLowerCase().contains(query)) {
-            return true
-        }
-
-        if (titleSearchRomaji != null && titleSearchRomaji!!.toLowerCase().contains(query)) {
-            return true
-        }
-
-        if (albums != null) {
-            for (album in albums!!) {
-                if (album.toLowerCase().contains(query)) {
-                    return true
-                }
-            }
-        }
-
-        if (albumsRomaji != null) {
-            for (album in albumsRomaji!!) {
-                if (album.toLowerCase().contains(query)) {
-                    return true
-                }
-            }
-        }
-
-        if (albumsSearchRomaji != null) {
-            for (album in albumsSearchRomaji!!) {
-                if (album.toLowerCase().contains(query)) {
-                    return true
-                }
-            }
-        }
-
-        if (artists != null) {
-            for (artist in artists!!) {
-                if (artist.toLowerCase().contains(query)) {
-                    return true
-                }
-            }
-        }
-
-        if (artistsRomaji != null) {
-            for (artist in artistsRomaji!!) {
-                if (artist.toLowerCase().contains(query)) {
-                    return true
-                }
-            }
-        }
-
-        if (artistsSearchRomaji != null) {
-            for (artist in artistsSearchRomaji!!) {
-                if (artist.toLowerCase().contains(query)) {
-                    return true
-                }
-            }
-        }
-
-        return false
+        return title.orEmpty().contains(query, ignoreCase = true)
+                || titleRomaji.orEmpty().contains(query, ignoreCase = true)
+                || titleSearchRomaji.orEmpty().contains(query, ignoreCase = true)
+                || artists.orEmpty().any { it.contains(query, ignoreCase = true) }
+                || artistsRomaji.orEmpty().any { it.contains(query, ignoreCase = true) }
+                || artistsSearchRomaji.orEmpty().any { it.contains(query, ignoreCase = true) }
+                || albums.orEmpty().any { it.contains(query, ignoreCase = true) }
+                || albumsRomaji.orEmpty().any { it.contains(query, ignoreCase = true) }
+                || albumsSearchRomaji.orEmpty().any { it.contains(query, ignoreCase = true) }
     }
 
     companion object {
         fun toSong(songListItem: SongListItem): Song {
-            val albums = ArrayList<SongDescriptor>()
-            for (i in 0 until songListItem.albums!!.size) {
-                val albumDescriptor = SongDescriptor()
-                albumDescriptor.id = songListItem.albumsId!![i]
-                albumDescriptor.name = songListItem.albums!![i]
-                albumDescriptor.nameRomaji = songListItem.albumsRomaji!![i]
-                albumDescriptor.image = songListItem.albumsCover!![i]
+            val songAlbums = songListItem.albums.orEmpty()
+                .mapIndexed { i, _ -> SongDescriptor().apply {
+                    id = songListItem.albumsId!![i]
+                    name = songListItem.albums!![i]
+                    nameRomaji = songListItem.albumsRomaji!![i]
+                    image = songListItem.albumsCover!![i]
+                }}
 
-                albums.add(albumDescriptor)
+            val songArtists = songListItem.artists.orEmpty()
+                .mapIndexed { i, _ -> SongDescriptor().apply {
+                    id = songListItem.artistsId!![i]
+                    name = songListItem.artists!![i]
+                    nameRomaji = songListItem.artistsRomaji!![i]
+                }}
+
+            val songSources = songListItem.sources.orEmpty()
+                    .mapIndexed { i, _ ->  SongDescriptor().apply {
+                        name = songListItem.sources!![i]
+                        nameRomaji = songListItem.sourcesRomaji!![i]
+                    }}
+
+            return Song().apply {
+                id = songListItem.id
+                title = songListItem.title
+                titleRomaji = songListItem.titleRomaji
+                albums = songAlbums
+                artists = songArtists
+                sources = songSources
+                duration = songListItem.duration
+                favorite = songListItem.favorite
             }
-
-            val artists = ArrayList<SongDescriptor>()
-            for (i in 0 until songListItem.artists!!.size) {
-                val artistDescriptor = SongDescriptor()
-                artistDescriptor.id = songListItem.artistsId!![i]
-                artistDescriptor.name = songListItem.artists!![i]
-                artistDescriptor.nameRomaji = songListItem.artistsRomaji!![i]
-
-                artists.add(artistDescriptor)
-            }
-
-            val sources = ArrayList<SongDescriptor>()
-            for (i in 0 until songListItem.sources!!.size) {
-                val sourceDescriptor = SongDescriptor()
-                sourceDescriptor.name = songListItem.sources!![i]
-                sourceDescriptor.nameRomaji = songListItem.sourcesRomaji!![i]
-
-                sources.add(sourceDescriptor)
-            }
-
-            val song = Song()
-            song.id = songListItem.id
-            song.title = songListItem.title
-            song.titleRomaji = songListItem.titleRomaji
-            song.albums = albums
-            song.artists = artists
-            song.sources = sources
-            song.duration = songListItem.duration
-            song.favorite = songListItem.favorite
-            song.enabled = songListItem.enabled
-
-            return song
         }
     }
 }
