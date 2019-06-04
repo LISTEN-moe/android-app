c9376262f75cb06054c436ec8621b09126fbb0fb
Eugene Cheung <ey.cheung@outlook.com>
2019-06-04 07:27:55 -0400

    Switch to beta socket for jpop

--- app/src/main/java/me/echeung/moemoekyun/client/api/library/Jpop.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/api/library/Jpop.kt
@@ -2,7 +2,7 @@ package me.echeung.moemoekyun.client.api.library
 
 class Jpop private constructor() : Library(
         NAME,
-        "wss://listen.moe/gateway",
+        "wss://listen.moe/beta_gateway",
         "https://listen.moe/fallback") {
     companion object {
         const val NAME = "jpop"
