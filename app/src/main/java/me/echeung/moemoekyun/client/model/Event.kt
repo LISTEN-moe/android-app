739417a3a666491d3294ce6c0533d2462584cc15
Eugene Cheung <ey.cheung@outlook.com>
2019-06-03 21:28:58 -0400

    Fix some nullability warnings

--- app/src/main/java/me/echeung/moemoekyun/client/model/Event.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/model/Event.kt
@@ -2,5 +2,5 @@ package me.echeung.moemoekyun.client.model
 
 data class Event(
         val name: String,
-        val image: String
+        val image: String?
 )
--- app/src/main/java/me/echeung/moemoekyun/client/model/User.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/model/User.kt
@@ -2,8 +2,8 @@ package me.echeung.moemoekyun.client.model
 
 data class User(
         val displayName: String,
-        val avatarImage: String,
-        val bannerImage: String,
+        val avatarImage: String?,
+        val bannerImage: String?,
 
         // Mutable field, to handle quickly updating in UI
         var requestsRemaining: Int
