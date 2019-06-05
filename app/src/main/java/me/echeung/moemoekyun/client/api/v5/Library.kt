fcc4844330648f8392133a4889fda4600a13dc81
Eugene Cheung <ey.cheung@outlook.com>
2019-06-04 22:38:05 -0400

    Minor edit

--- app/src/main/java/me/echeung/moemoekyun/client/api/v5/Library.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/api/v5/Library.kt
@@ -1,6 +1,6 @@
 package me.echeung.moemoekyun.client.api.v5
 
-abstract class Library(val name: String, val socketUrl: String, val streamUrl: String) {
+abstract class Library(val name: String) {
     companion object {
         const val API_BASE = "https://listen.moe/graphql"
     }
