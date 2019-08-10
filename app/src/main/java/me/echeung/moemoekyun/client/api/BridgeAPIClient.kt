360502ffc5f85778756285f5adaad86424f1b5fd
Eugene <ey.cheung@outlook.com>
2019-08-10 18:20:38 -0400

    Janky way to force users to log out when we switch to the v5 API

--- app/src/main/java/me/echeung/moemoekyun/client/api/BridgeAPIClient.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/api/BridgeAPIClient.kt
@@ -23,42 +23,50 @@ class BridgeAPIClient(okHttpClient: OkHttpClient, authUtil: AuthTokenUtil) : API
     private val api5: APIClient5 = APIClient5(okHttpClient, authUtil)
 
     override fun authenticate(username: String, password: String, callback: LoginCallback) {
-        api4.authenticate(username, password, callback)
+        getApi().authenticate(username, password, callback)
     }
 
     override fun authenticateMfa(otpToken: String, callback: LoginCallback) {
-        api4.authenticateMfa(otpToken, callback)
+        getApi().authenticateMfa(otpToken, callback)
     }
 
     override fun register(email: String, username: String, password: String, callback: RegisterCallback) {
-        api4.register(email, username, password, callback)
+        getApi().register(email, username, password, callback)
     }
 
     override fun getUserInfo(callback: UserInfoCallback) {
-        api4.getUserInfo(callback)
+        getApi().getUserInfo(callback)
     }
 
     override fun getUserFavorites(callback: UserFavoritesCallback) {
-        api4.getUserFavorites(callback)
+        getApi().getUserFavorites(callback)
     }
 
     override fun isFavorite(songIds: List<Int>, callback: IsFavoriteCallback) {
-        api4.isFavorite(songIds, callback)
+        getApi().isFavorite(songIds, callback)
     }
 
     override fun toggleFavorite(songId: Int, isFavorite: Boolean, callback: FavoriteSongCallback) {
-        api4.toggleFavorite(songId, isFavorite, callback)
+        getApi().toggleFavorite(songId, isFavorite, callback)
     }
 
     override fun requestSong(songId: Int, callback: RequestSongCallback) {
-        api4.requestSong(songId, callback)
+        getApi().requestSong(songId, callback)
     }
 
     override fun getSongs(callback: SongsCallback) {
-        api4.getSongs(callback)
+        getApi().getSongs(callback)
     }
 
     override fun search(query: String?, callback: SearchCallback) {
-        api4.search(query, callback)
+        getApi().search(query, callback)
+    }
+
+    private fun getApi(): APIClient {
+        return if (useApi5) api5 else api4
+    }
+
+    companion object {
+        const val useApi5: Boolean = true
     }
 }
--- app/src/main/java/me/echeung/moemoekyun/client/auth/AuthTokenUtil.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/auth/AuthTokenUtil.kt
@@ -2,6 +2,7 @@ package me.echeung.moemoekyun.client.auth
 
 import android.content.Context
 import androidx.preference.PreferenceManager
+import me.echeung.moemoekyun.client.api.BridgeAPIClient
 
 import java.lang.ref.WeakReference
 import kotlin.math.roundToInt
@@ -51,6 +52,15 @@ class AuthTokenUtil(context: Context) {
                     .putString(USER_TOKEN, token)
                     .putLong(LAST_AUTH, System.currentTimeMillis() / 1000)
                     .apply()
+
+            // TODO: temporarily tracking whether we've logged in using the new API or not to force
+            // users authenticated with the v4 API to log out
+            if (BridgeAPIClient.useApi5) {
+                PreferenceManager.getDefaultSharedPreferences(context)
+                        .edit()
+                        .putBoolean(IS_V5_AUTHED, true)
+                        .apply()
+            }
         }
 
     /**
@@ -96,6 +106,15 @@ class AuthTokenUtil(context: Context) {
             return false
         }
 
+        // TODO: force log out v4 users if switched to v5 API
+        if (BridgeAPIClient.useApi5) {
+            val context = contextRef.get()
+            if (context == null || !PreferenceManager.getDefaultSharedPreferences(context).getBoolean(IS_V5_AUTHED, false)) {
+                clearAuthToken()
+                return false
+            }
+        }
+
         return true
     }
 
@@ -126,5 +145,7 @@ class AuthTokenUtil(context: Context) {
     companion object {
         private const val USER_TOKEN = "user_token"
         private const val LAST_AUTH = "last_auth"
+
+        private const val IS_V5_AUTHED = "v5_authed"
     }
 }
