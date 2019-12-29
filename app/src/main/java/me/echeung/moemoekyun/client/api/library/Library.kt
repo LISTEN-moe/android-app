7513445b149fa44f50584cdfd4cb3bfc293cfc31
arkon <eugcheung94@gmail.com>
2019-12-29 15:20:42 -0500

    Add subscriptions endpoint

--- app/src/main/java/me/echeung/moemoekyun/client/api/library/Library.kt
+++ app/src/main/java/me/echeung/moemoekyun/client/api/library/Library.kt
@@ -3,6 +3,7 @@ package me.echeung.moemoekyun.client.api.library
 abstract class Library(val name: String, val socketUrl: String, val streamUrl: String) {
     companion object {
         const val API_BASE = "https://listen.moe/graphql"
+        const val API_SUBCRIPTIONS = "wss://listen.moe/subscriptions"
 
         const val CDN_ALBUM_ART_URL = "https://cdn.listen.moe/covers/"
         const val CDN_AVATAR_URL = "https://cdn.listen.moe/avatars/"
