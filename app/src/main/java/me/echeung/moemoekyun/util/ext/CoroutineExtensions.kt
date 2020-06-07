65297660ab69094b188e436d4e9bd6747b7ee156
arkon <eugcheung94@gmail.com>
2020-06-07 13:03:51 -0400

    Fix crash when starting app without network

--- app/src/main/java/me/echeung/moemoekyun/util/ext/CoroutineExtensions.kt
+++ app/src/main/java/me/echeung/moemoekyun/util/ext/CoroutineExtensions.kt
@@ -4,16 +4,18 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
+private val scope = CoroutineScope(SupervisorJob())
+
 fun launchUI(block: suspend CoroutineScope.() -> Unit): Job =
-    GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT, block)
+    scope.launch(Dispatchers.Main, CoroutineStart.DEFAULT, block)
 
 fun launchIO(block: suspend CoroutineScope.() -> Unit): Job =
-    GlobalScope.launch(Dispatchers.IO, CoroutineStart.DEFAULT, block)
+    scope.launch(Dispatchers.IO, CoroutineStart.DEFAULT, block)
 
 @OptIn(ExperimentalCoroutinesApi::class)
 fun launchNow(block: suspend CoroutineScope.() -> Unit): Job =
-    GlobalScope.launch(Dispatchers.Main, CoroutineStart.UNDISPATCHED, block)
+    scope.launch(Dispatchers.Main, CoroutineStart.UNDISPATCHED, block)
