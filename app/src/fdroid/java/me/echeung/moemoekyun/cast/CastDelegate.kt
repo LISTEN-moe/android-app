b54525849e1b48bd996e19e1d297dda99a7d1cf3
arkon <eugcheung94@gmail.com>
2020-06-13 11:07:24 -0400

    Hide cast button in F-Droid builds

--- app/src/fdroid/java/me/echeung/moemoekyun/cast/CastDelegate.kt
+++ app/src/fdroid/java/me/echeung/moemoekyun/cast/CastDelegate.kt
@@ -2,6 +2,10 @@ package me.echeung.moemoekyun.cast
 
 import android.content.Context
 import android.view.Menu
+import me.echeung.moemoekyun.R
+import me.echeung.moemoekyun.client.api.socket.Socket
+import me.echeung.moemoekyun.client.stream.Stream
+import me.echeung.moemoekyun.viewmodel.RadioViewModel
 
 /**
  * No-op versions of functions used in Play Store version for Google Cast support
@@ -17,5 +21,6 @@ class CastDelegate(
     }
 
     fun initCastButton(menu: Menu?) {
+        menu?.findItem(R.id.media_route_menu_item)?.isVisible = false
     }
 }
