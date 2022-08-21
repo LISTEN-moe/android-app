c1328091f887337a421f889fb7bb8007dadf8372
arkon <eugcheung94@gmail.com>
2022-08-21 17:35:29 -0400

    Convert app module build.gradle to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,150 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("plugin.serialization")
+    id("com.apollographql.apollo3") version "3.5.0"
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "me.echeung.moemoekyun"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 105
+        versionName = "5.4.2"
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    buildTypes {
+        named("debug") {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = " DEBUG"
+        }
+        named("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    flavorDimensions.add("variant")
+    productFlavors {
+        create("playstore") {
+            dimension = "variant"
+        }
+        create("fdroid") {
+            dimension = "variant"
+
+            applicationIdSuffix = ".fdroid"
+        }
+    }
+
+    lint {
+        disable.addAll(listOf("MissingTranslation", "ExtraTranslation"))
+        enable.addAll(listOf("ObsoleteSdkInt"))
+
+        isAbortOnError = true
+    }
+
+    packagingOptions {
+        resources.excludes.addAll(listOf(
+            "META-INF/DEPENDENCIES",
+            "LICENSE.txt",
+            "META-INF/LICENSE",
+            "META-INF/LICENSE.txt",
+            "META-INF/README.md",
+            "META-INF/NOTICE",
+            "META-INF/*.kotlin_module",
+            "META-INF/*.version",
+        ))
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    val coroutinesVersion = "1.5.2"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    implementation("io.insert-koin:koin-android:3.2.0")
+
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.2")
+    implementation("androidx.media:media:1.6.0")
+    implementation("androidx.palette:palette-ktx:1.0.0")
+    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+
+    implementation("com.google.android.material:material:1.4.0")
+
+    implementation("com.github.tfcporciuncula:flow-preferences:1.3.4")
+
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+
+    val apolloVersion = "3.5.0"
+    implementation("com.apollographql.apollo3:apollo-runtime:$apolloVersion")
+    implementation("com.apollographql.apollo3:apollo-http-cache:$apolloVersion")
+
+    val glideVersion = "4.12.0"
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    kapt("com.github.bumptech.glide:compiler:$glideVersion")
+
+    val exoplayerVersion = "2.18.1"
+    implementation("com.google.android.exoplayer:exoplayer-core:$exoplayerVersion")
+    "playstoreImplementation"("com.google.android.exoplayer:extension-cast:$exoplayerVersion")
+
+    // Google Cast support (not available in fdroid flavour)
+    "playstoreImplementation"("com.google.android.gms:play-services-cast-framework:21.1.0")
+
+    implementation("de.psdev.licensesdialog:licensesdialog:2.1.0")
+
+    // For detecting memory leaks; see https://square.github.io/leakcanary/
+    // "debugImplementation"("com.squareup.leakcanary:leakcanary-android:2.2")
+}
+
+apollo {
+    packageName.set("me.echeung.moemoekyun")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        // See https://kotlinlang.org/docs/reference/experimental.html#experimental-status-of-experimental-api-markers
+        kotlinOptions.freeCompilerArgs += listOf(
+            "-Xopt-in=kotlin.Experimental",
+            "-Xopt-in=kotlin.RequiresOptIn",
+            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-Xuse-experimental=kotlinx.coroutines.FlowPreview",
+        )
+    }
+
+    preBuild {
+        dependsOn(formatKotlin)
+    }
+}
