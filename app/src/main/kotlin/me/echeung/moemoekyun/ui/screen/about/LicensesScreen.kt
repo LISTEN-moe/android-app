5652d17e82dd178ee2826a4dc86c252c9d8393e5
arkon <arkon@users.noreply.github.com>
2024-02-16 17:03:30 -0500

    Bump dependencies

--- app/src/main/kotlin/me/echeung/moemoekyun/ui/screen/about/LicensesScreen.kt
+++ app/src/main/kotlin/me/echeung/moemoekyun/ui/screen/about/LicensesScreen.kt
@@ -1,13 +1,11 @@
 package me.echeung.moemoekyun.ui.screen.about
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
-import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
-import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
+import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import me.echeung.moemoekyun.R
 import me.echeung.moemoekyun.ui.common.Toolbar
 
@@ -22,12 +20,6 @@ class LicensesScreen : Screen {
                 modifier = Modifier
                     .fillMaxSize(),
                 contentPadding = contentPadding,
-                colors = LibraryDefaults.libraryColors(
-                    backgroundColor = MaterialTheme.colorScheme.background,
-                    contentColor = MaterialTheme.colorScheme.onBackground,
-                    badgeBackgroundColor = MaterialTheme.colorScheme.primary,
-                    badgeContentColor = MaterialTheme.colorScheme.onPrimary,
-                ),
             )
         }
     }
