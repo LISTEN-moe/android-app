af29d92449ebbd800b9cf533d590824e28663c42
arkon <arkon@users.noreply.github.com>
2024-03-31 17:33:41 -0400

    Avoid search results being covered by nav bar

--- app/src/main/kotlin/me/echeung/moemoekyun/ui/screen/search/SearchScreen.kt
+++ app/src/main/kotlin/me/echeung/moemoekyun/ui/screen/search/SearchScreen.kt
@@ -2,8 +2,13 @@ package me.echeung.moemoekyun.ui.screen.search
 
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -66,7 +71,9 @@ object SearchScreen : Screen {
                         }
                     },
                 ) {
-                    LazyColumn {
+                    LazyColumn(
+                        modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Bottom)),
+                    ) {
                         songsItems(
                             songs = state.filteredSongs,
                             showFavoriteIcons = true,
