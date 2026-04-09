89ada5534fe0e1429c8a7303ea4b2f70d60935a9
arkon <arkon@users.noreply.github.com>
2026-04-08 22:44:10 -0400

    Release v7.0.0-beta.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = appPackageName
         minSdk = 26
         targetSdk = 36
-        versionCode = 213
-        versionName = "7.0.0-beta.2"
+        versionCode = 214
+        versionName = "7.0.0-beta.3"
     }
 
     buildFeatures {
